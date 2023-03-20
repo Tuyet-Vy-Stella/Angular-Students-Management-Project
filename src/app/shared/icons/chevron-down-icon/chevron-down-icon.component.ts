b5b44e6d037242427de7683b35d7fdb7286f4e98
nttruong21 <nguyenthetruong100621@gmail.com>
2023-03-20 10:15:57 +0700

    Handle UI: Finish Header & Sidebar

---
+++ src/app/shared/icons/chevron-down-icon/chevron-down-icon.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chevron-down-icon',
+  templateUrl: './chevron-down-icon.component.html',
+  styleUrls: ['./chevron-down-icon.component.scss']
+})
+export class ChevronDownIconComponent {
+
+}
