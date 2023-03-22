f43d90dcc3c2900c7f207cf5ab2048d57bf6b543
LuongLeVan <levanluong6a8@gmail.com>
2023-03-22 14:14:45 +0700

    Change title

--- src/app/subject/service/subject.service.ts
+++ src/app/subject/service/subject.service.ts
@@ -7,11 +7,13 @@ import { Observable } from 'rxjs';
 })
 export class SubjectService implements OnInit {
   nameSubject: string = '';
+  subjectList:any = [];
 
   constructor(private http: HttpClient) { }
 
   getListSubject(): Observable<any> {
     return this.http.get('https://qlsv-mu.vercel.app/api/subject-list');
+
   }
 
   /* Create new Subject */
@@ -23,11 +25,11 @@ export class SubjectService implements OnInit {
           address: "8204 Oak Avenue, Newark, NJ, 07104",
           birthday: "2001-09-11",
           created_at: "2023-03-20T06:38:00.249710",
-          email: "levanluongk15.com",
+          email: "robert.jones@example.com",
           gender: "male",
           id: Math.floor(Math.random() * 10000) + 1,
           joined_date: "2001-05-22",
-          name: "Eddie ",
+          name: "Eddie",
           password: "$2b$12$hBcOaOTgk6KOr4UgwEKTJuJlksdLvN4y0Mu39qABWyhSAggzn0B/i",
           phone: "(973) 555-1212"
         }
@@ -38,12 +40,12 @@ export class SubjectService implements OnInit {
   
   /* Filter Subject by id */
   getSubjectById(id: number): Observable<any> {
-    return this.http.get(`https://qlsv-mu.vercel.app/api/subject/?subject_id=${id}`)
+     return this.http.get(`https://qlsv-mu.vercel.app/api/subject/?subject_id=${id}`)
   }
 
 
   /* Set name for edit page*/
-  setNameEdit(name: string) {
+  setNameEdit(name: string) {    
     this.nameSubject = name;
   }
 
--- src/app/subject/subject-list/subject-list.component.ts
+++ src/app/subject/subject-list/subject-list.component.ts
@@ -8,84 +8,106 @@ import { SubjectService } from 'src/app/subject/service/subject.service';
   styleUrls: ['./subject-list.component.scss']
 })
 export class SubjectListComponent implements OnInit {
-  subjectList:any[] = [];
-  name:string = '';
-  nameSearchText:string = '';
-  id:any = '';
+  subjectList: any[] = [];
+  name: string = '';
+  nameSearchText: string = '';
+  id: any = '';
   @ViewChild('createInput') inputEl!: ElementRef;
 
   //Focus Input
-  FocusInput(){
+  FocusInput() {
     this.inputEl.nativeElement.focus();
   }
-  
-  constructor(private SubjectService: SubjectService, private title: Title){}
+
+  constructor(private SubjectService: SubjectService, private title: Title) { }
 
   ngOnInit() {
     this.title.setTitle('Subject List')
     this.SubjectService.getListSubject().subscribe(res => {
-      this.subjectList = res;      
+      this.subjectList = res;
     })
   }
 
   /* Handle Create */
-  handleCreate(name:string){
-    this.SubjectService.createSubject(name).subscribe(res => {
+  handleCreate(name: string) {
+    if (name == '') {
+      alert('Please fill out input value.');
+    } else {
+      this.SubjectService.createSubject(name).subscribe(res => {
         this.subjectList = [...this.subjectList, res];
-        
-    })
+
+      })
+
+    }
+
 
 
   }
 
   /* Filter by Id */
-  handleFilter(id:number){
-    if(this.id === '' ) {
+  handleFilter(id: number) {
+    if (this.id === '') {
       this.SubjectService.getListSubject().subscribe(res => {
         this.subjectList = res;
-        
-        
-      }) 
+      })
+
+
     }
-    this.SubjectService.getSubjectById(id).subscribe(res => {
-      this.subjectList = [res];
-      
-    })
+    else {
+
+      this.SubjectService.getSubjectById(id).subscribe(res => {
+        this.subjectList = [res];
+
+      })
+    }
+
+
+
   }
 
   /* Filter by Name */
-  handleFilterByName(name:string){
-    this.SubjectService.getListSubject().subscribe(res => {
-      this.subjectList = res.filter((s:any) => {
-        return s.name.toLowerCase().includes(name.toLowerCase())
-         })
+  handleFilterByName(name: string) {
+    if (name !== '' && this.id !== '') {
+      this.SubjectService.getListSubject().subscribe(res => {
+        this.subjectList = res.filter((s: any) => {
+          return s.name.toLowerCase().includes(name.toLowerCase()) && s.id == this.id
+        })
 
-    })
-    
+      })
+    } else {
+
+      this.SubjectService.getListSubject().subscribe(res => {
+        this.subjectList = res.filter((s: any) => {
+          return s.name.toLowerCase().includes(name.toLowerCase())
+        })
+
+      })
+    }
+
   }
 
-  handleUpdate(name:string){
-      this.SubjectService.setNameEdit(name);
+  handleUpdate(name: string) {
+    this.SubjectService.setNameEdit(name);
   }
 
   onSelect(event: any) {
     const selectedValue = event.target.value;
-    if(selectedValue === 'All'){
+    if (selectedValue === 'All') {
       this.SubjectService.getListSubject().subscribe(res => {
-       this.subjectList = res;
-       
-        
+        this.subjectList = res;
+
+
       })
-    }else {
+    } else {
       this.SubjectService.getListSubject().subscribe(res => {
-        this.subjectList = res.slice(0,+selectedValue);
-        
-         
-       })
+        this.subjectList = res.slice(0, +selectedValue);
+
+
+      })
     }
 
 
   }
 
- 
+
 }
