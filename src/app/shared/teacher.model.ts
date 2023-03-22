e93aa41dd8bcaacf2af535a4aa8fc70991a553d7
Duy Minh <duyminh081201@gmail.com>
2023-03-23 01:56:23 +0700

    Update teacher.model.ts

--- src/app/shared/teacher.model.ts
+++ src/app/shared/teacher.model.ts
@@ -1,13 +1,14 @@
-export class Teacher {
-  constructor(
-    public email : string,
-    public password : string,
-    public name : string,
-    public gender : number,
-    public dateOfBirth : Date,
-    public phone : string,
-    public address : string,
-    public joiningDate : string
-  ) {
-  }
+export interface Teacher {
+  id: number
+  name: string
+  email: string
+  gender: string
+  birthday: string
+  address: string
+  phone: string
+  joined_date: string
+  subject_id: number
+  created_at: string
 }
+
+export type CreateTeacherModel = Omit<Teacher, 'id' | 'createdAt'>
