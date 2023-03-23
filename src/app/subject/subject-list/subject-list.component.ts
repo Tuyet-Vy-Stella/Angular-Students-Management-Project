7883ad0789ec1fd479289b87a32eb1a3d8d91443
Tuyết Vy (Stella) <nhuochanvu@gmail.com>
2023-03-23 23:22:06 +0700

    Revert "Merge branch 'subject' into components"
    This reverts commit 4efd477fa3ede90787a69ad85fb2de443f921362, reversing
    changes made to b6f602a21aff41effc61857a250367acae688a57.

--- src/app/subject/subject-list/subject-list.component.ts
+++ src/app/subject/subject-list/subject-list.component.ts
@@ -1,6 +1,5 @@
-import { Component, ViewChild, ElementRef, OnInit, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
-import { Subject } from 'src/app/shared/subject.model';
 import { SubjectService } from 'src/app/subject/service/subject.service';
 
 @Component({
@@ -8,13 +7,12 @@ import { SubjectService } from 'src/app/subject/service/subject.service';
   templateUrl: './subject-list.component.html',
   styleUrls: ['./subject-list.component.scss']
 })
-export class SubjectListComponent implements OnInit, OnChanges {
-  subjectList: Subject[] = [];
+export class SubjectListComponent implements OnInit {
+  subjectList: any[] = [];
   name: string = '';
   nameSearchText: string = '';
   id: any = '';
   @ViewChild('createInput') inputEl!: ElementRef;
-  
 
   //Focus Input
   FocusInput() {
@@ -24,32 +22,22 @@ export class SubjectListComponent implements OnInit, OnChanges {
   constructor(private SubjectService: SubjectService, private title: Title) { }
 
   ngOnInit() {
-    this.title.setTitle('Subject List');
+    this.title.setTitle('Subject List')
     this.SubjectService.getListSubject().subscribe(res => {
       this.subjectList = res;
 
+      console.log(res);
+      
     })
   }
-
-
-  /* Handle if not found */
-  handleList() {
-    if (this.id === '' && this.nameSearchText === '') {
-      this.SubjectService.getListSubject().subscribe(res => {
-        this.subjectList = res;
-      })
-    }
-  }
-
 
   /* Handle Create */
   handleCreate(name: string) {
     if (name == '') {
       alert('Please fill out input value.');
     } else {
-      alert('Create is successfull.')
       this.SubjectService.createSubject(name).subscribe(res => {
-        this.subjectList = [...this.subjectList, {name: name}];
+        this.subjectList = [...this.subjectList, res];
 
       })
 
@@ -60,76 +48,76 @@ export class SubjectListComponent implements OnInit, OnChanges {
 
 
   /* Filter by Id */
-  /*   handleFilter(id: number) {
-      if (this.id === '') {
-        this.SubjectService.getListSubject().subscribe(res => {
-          this.subjectList = res;
-        })
-  
-  
-      }
-      else {
-  
-        this.SubjectService.getSubjectById(id).subscribe(res => {
-          this.subjectList = [res];
-  
-        })
-      }
-  
-  
-  
-    } */
+/*   handleFilter(id: number) {
+    if (this.id === '') {
+      this.SubjectService.getListSubject().subscribe(res => {
+        this.subjectList = res;
+      })
+
+
+    }
+    else {
+
+      this.SubjectService.getSubjectById(id).subscribe(res => {
+        this.subjectList = [res];
+
+      })
+    }
+
+
+
+  } */
 
   /* Filter by Name */
-  /*   handleFilterByName(name: string) {
-      if (name !== '' && this.id !== '') {
-        this.SubjectService.getListSubject().subscribe(res => {
-          this.subjectList = res.filter((s: any) => {
-            return s.name.toLowerCase().includes(name.toLowerCase()) && s.id == this.id
-          })
-  
+/*   handleFilterByName(name: string) {
+    if (name !== '' && this.id !== '') {
+      this.SubjectService.getListSubject().subscribe(res => {
+        this.subjectList = res.filter((s: any) => {
+          return s.name.toLowerCase().includes(name.toLowerCase()) && s.id == this.id
         })
-      } else {
-  
-        this.SubjectService.getListSubject().subscribe(res => {
-          this.subjectList = res.filter((s: any) => {
-            return s.name.toLowerCase().includes(name.toLowerCase())
-          })
-  
+
+      })
+    } else {
+
+      this.SubjectService.getListSubject().subscribe(res => {
+        this.subjectList = res.filter((s: any) => {
+          return s.name.toLowerCase().includes(name.toLowerCase())
         })
-      }
-  
-    } */
+
+      })
+    }
+
+  } */
 
   handleUpdate(name: string) {
     this.SubjectService.setNameEdit(name);
   }
 
   /* Handle Search */
-  handleSearch() {
+  handleSearch(){
 
     /* Handle search if input value of id and value of name not empty */
-    if (this.id.length !== 0 && this.nameSearchText.length !== 0) {
-
+    if(this.id.length !== 0 && this.nameSearchText.length !== 0){
+      
       this.SubjectService.getListSubject().subscribe(res => {
-        this.subjectList = res.filter((sub: Subject) => sub.id === +this.id && sub.name.toLowerCase().includes(this.nameSearchText.toLowerCase()))
+        this.subjectList = res.filter((sub:any) => sub.id === +this.id && sub.name.toLowerCase().includes(this.nameSearchText.toLowerCase()))
       })
 
     }
 
     /* Handle search if input value of id not empty and name is empty */
-    if (this.id.length !== 0 && this.nameSearchText.length === 0) {
-
+    if(this.id.length !== 0 && this.nameSearchText.length === 0){
+      
       this.SubjectService.getListSubject().subscribe(res => {
-        this.subjectList = res.filter((sub: Subject) => sub.id === +this.id)
+        this.subjectList = res.filter((sub:any) => sub.id === +this.id)
       })
     }
 
     /* Handle search if input value of id is empty and name is not empty */
-    if (this.id.length === 0 && this.nameSearchText.length !== 0) {
-
+    if(this.id.length === 0 && this.nameSearchText.length !== 0){
+      
       this.SubjectService.getListSubject().subscribe(res => {
-        this.subjectList = res.filter((sub: Subject) => sub.name.toLowerCase().includes(this.nameSearchText.toLowerCase()))
+        this.subjectList = res.filter((sub:any) => sub.name.toLowerCase().includes(this.nameSearchText.toLowerCase()))
       })
     }
 
@@ -156,11 +144,6 @@ export class SubjectListComponent implements OnInit, OnChanges {
       })
     }
 
-
-  }
-
-
-  ngOnChanges(changes: SimpleChanges): void {
 
   }
 
